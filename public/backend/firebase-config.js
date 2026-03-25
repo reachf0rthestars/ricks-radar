ef7b659a52e0b16860d1f5e7aa43d3193aaa8600
AustinConner <a.l.conner.2002@gmail.com>
2026-03-25 10:44:00 -0600

    changed imports.

--- public/backend/firebase-config.js
+++ public/backend/firebase-config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: import.meta.env.FIREBASE_CONFIG_API_KEY,
