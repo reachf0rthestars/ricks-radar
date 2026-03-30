cf9116a4c5a8a0d4699da36314f380f743d42321
Sawyer Linginfelter <sawking3114@gmail.com>
2026-03-30 11:16:17 -0600

    idek dawg

--- public/backend/deals-service.js
+++ public/backend/deals-service.js
@@ -1,5 +1,13 @@
 // Create a new deal
 const database = firebase.database();
+let title = document.querySelector(".title")
+let amount = document.querySelector(".amount")
+let description = document.querySelector(".description")
+let categories = document.querySelector("#selected-categories")
+let location = document.querySelector(".location")
+let time = document.querySelector(".time")
+let expiration = document.querySelector("#expiration-date")
+let image = document.querySelector("#image")
 
 
 
--- public/backend/firebase-config.js
+++ public/backend/firebase-config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
 import { 
   getFirestore, 
   collection, 
